b08f0ccdcbf4b9107517b7ecfb06037de1ddf297
Max Lindahl <Max.Lindahl97@gmail.com>
2021-09-06 08:47:18 +0200

    Added comments
    Comments were added for increased understanding.

--- src/CalculatorScript.js
+++ src/CalculatorScript.js
@@ -1,13 +1,20 @@
+//variables to track entered numbers and operators
 var number1;
 var operator;
 var number2;
+//save the calculated answer
 var answer;
+//booleans to track the current state of the program
 var isFirstNumberDone = false;
 var isNumberOperatorEntered = false;
 var isCalculationComplete = false;
 var isCommaEntered = false;
+//used to calculate the decimal value of the entered number
 var numberAfterComma = 1;
 
+/*
+Updates the display on the calculator with up to date information giving the user real time responses.
+*/
 function updateText(){
 	if(isCalculationComplete){
 		document.getElementById("outputText").innerHTML = number1 + " " + operator + " " + number2 + " = " + answer;
@@ -22,6 +29,11 @@ function updateText(){
 	}
 }
 
+/*
+Called when a number is clicked on, updates the current number being worked on with the entered number
+
+@parameter: the number that was clicked on
+*/
 function enterNumber(number){
 	if(!isCalculationComplete){
 		if(!isFirstNumberDone){
@@ -66,14 +78,25 @@ function enterNumber(number){
 	}
 }
 
+/*
+Called when the user clicks on the dot
+Sets the state of the program to handle the next numbers entered as if they are decimals 
+*/
 function enterComma(){
 	if(!isCommaEntered){
 		isCommaEntered = true;
 	}else{
+		//todo
 		//if comma is alrdy active do some animation to tell the user
 	}
 }
 
+/*
+Called when the user clicks on an operator
+if the operator is '=' calculate the answer and display it
+if any other operator is clicked set that operator as the calculations operator 
+and set the programs state to start calculating the second number
+*/
 function enterOperator(newOperator){
 	if(newOperator == '=' && !isCalculationComplete){
 		if(number1 == null || operator == null || number2 == null){
@@ -113,7 +136,10 @@ function enterOperator(newOperator){
 	
 }
 
-//returns the original color to the operator buttons
+/*
+Called when a operator button is pressed but the calculation already has a selected operator
+turns the operator buttons red for a short while to indicate to the user that an invalid input was made
+*/
 function turnOpColorsRedAndBack(){
 	//turn all op buttons red
 		var buttons = document.getElementsByClassName("operator");
@@ -123,6 +149,9 @@ function turnOpColorsRedAndBack(){
 		//turn them back after 200ms
 		setTimeout(turnOpColorsBack, 200);
 }
+/*
+turns the operator buttons color back to their original color
+*/
 function turnOpColorsBack(){
 	var buttons = document.getElementsByClassName("operator");
 	for(let i = 0; i < buttons.length; i++){
@@ -130,6 +159,9 @@ function turnOpColorsBack(){
 	}
 }
 
+/*
+resets the state of the program and all variables so that the user can do a new calculation
+*/
 function reset(){
 	number1 = null;
 	number2 = null;
